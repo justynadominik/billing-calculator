9fa1b7429cda03b762fca0e6084af13ecf5a6791
Konrad Wędzicha <konrad.wedzicha@relativity.com>
2023-07-31 12:51:48 +0200

    small improvemnts

--- Source/BillingCalculator.CustomPage/vite.config.ts
+++ Source/BillingCalculator.CustomPage/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "url";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
@@ -13,4 +14,12 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
 });
