6885d52117f94ad709c5d7dcc2271b28b2d9b1fa
Darina Kavokina <darina.kavokina@relativity.com>
2023-08-01 10:39:30 +0200

    Add collapcable category with sliders

--- Source/BillingCalculator.CustomPage/src/components/Repository/RepositoryComponent.vue
+++ Source/BillingCalculator.CustomPage/src/components/Repository/RepositoryComponent.vue
@@ -8,6 +8,84 @@
             <span class = "span1"></span>
             <rwc-text-input-field label="Text Only Documents" value="" edit-mode ></rwc-text-input-field>
         </span>
+        <div class="div1">
+            <rwc-category category-title="Bilable File Size Breakdown" collapsible defined reduced-spacing>
+            <table>
+                <tr>
+                  <td>
+                    <static-text class="static-text"><b>Billable File Size</b></static-text>
+                    <rwc-slider class="slider-style" max="100" step="5" length="300px" orientation="horizontal">
+                    </rwc-slider>
+                  </td>
+                  <td>
+                    <rwc-number-input-field label="GB" value="" edit-mode
+                        style="padding-right: 0px; margin-right: 0px; border-spacing: 0px;" >
+                    </rwc-number-input-field>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <static-text class="static-text"><b>Billable File Size</b></static-text>
+                    <rwc-slider class="slider-style" max="100" step="5" length="300px" orientation="horizontal">
+                    </rwc-slider>
+                  </td>
+                  <td>
+                    <rwc-number-input-field label="GB" value="" edit-mode
+                        style="padding-right: 0px; margin-right: 0px; border-spacing: 0px;" >
+                    </rwc-number-input-field>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <static-text class="static-text"><b>Billable File Size</b></static-text>
+                    <rwc-slider class="slider-style" max="100" step="5" length="300px" orientation="horizontal">
+                    </rwc-slider>
+                  </td>
+                  <td>
+                    <rwc-number-input-field label="GB" value="" edit-mode
+                        style="padding-right: 0px; margin-right: 0px; border-spacing: 0px;" >
+                    </rwc-number-input-field>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <static-text class="static-text"><b>Billable File Size</b></static-text>
+                    <rwc-slider class="slider-style" max="100" step="5" length="300px" orientation="horizontal">
+                    </rwc-slider>
+                  </td>
+                  <td>
+                    <rwc-number-input-field label="GB" value="" edit-mode
+                        style="padding-right: 0px; margin-right: 0px; border-spacing: 0px;" >
+                    </rwc-number-input-field>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <static-text class="static-text"><b>Billable File Size</b></static-text>
+                    <rwc-slider class="slider-style" max="100" step="5" length="300px" orientation="horizontal">
+                    </rwc-slider>
+                  </td>
+                  <td>
+                    <rwc-number-input-field label="GB" value="" edit-mode
+                        style="padding-right: 0px; margin-right: 0px; border-spacing: 0px;" >
+                    </rwc-number-input-field>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <static-text class="static-text"><b>Billable File Size</b></static-text>
+                    <rwc-slider class="slider-style" max="100" step="5" length="300px" orientation="horizontal">
+                    </rwc-slider>
+                  </td>
+                  <td>
+                    <rwc-number-input-field label="GB" value="" edit-mode
+                        style="padding-right: 0px; margin-right: 0px; border-spacing: 0px;" >
+                    </rwc-number-input-field>
+                  </td>
+                </tr>
+            </table>
+        </rwc-category>
+        </div>
     </rwc-category>
 </template>
 
