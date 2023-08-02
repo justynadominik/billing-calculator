28bbb672f6242584bde82cd0827e8e0f865fa191
Piotr Szydło <piotr.szydlo@relativity.com>
2023-08-02 10:41:23 +0200

    add link

--- Source/BillingCalculator.CustomPage/src/components/ChartConfiguration.ts
+++ Source/BillingCalculator.CustomPage/src/components/ChartConfiguration.ts
@@ -5,14 +5,16 @@ export class ChartConfiguration {
 
     reviewSeries: am5xy.LineSeries;
     repoSeries: am5xy.LineSeries;
-    sumSeries: am5xy.LineSeries;
+    linkReviewSeries: am5xy.LineSeries;
     reviewData: Point[] = [];
     repoData: Point[] = [];
-    sumData: Point[] = [];
+    linkReviewData: Point[] = [];
     constructor() {
         const root = am5.Root.new("chartdiv");
         let chart = root.container.children.push(
-            am5xy.XYChart.new(root, {})
+            am5xy.XYChart.new(root, {
+                layout: root.verticalLayout
+            })
         );
 
         let yAxis = chart.yAxes.push(
@@ -20,6 +22,14 @@ export class ChartConfiguration {
                 renderer: am5xy.AxisRendererY.new(root, {})
             })
         );
+        yAxis.children.unshift(
+            am5.Label.new(root, {
+              rotation: -90,
+              text: "File size, GB",
+              y: am5.p50,
+              centerX: am5.p50
+            })
+          );
 
         let xAxis = chart.xAxes.push(
             am5xy.DateAxis.new(root, {
@@ -30,14 +40,31 @@ export class ChartConfiguration {
                 }
             })
         );
+        let legend = chart.children.push(am5.Legend.new(root, {
+
+        }));
 
         this.reviewSeries = chart.series.push(
             am5xy.LineSeries.new(root, {
-                name: "Series",
+                name: "Review",
                 xAxis: xAxis,
                 yAxis: yAxis,
                 valueYField: "value",
                 valueXField: "date",
+                stroke: am5.color("#7eb0d5"),
+                tooltip: am5.Tooltip.new(root, {
+                    labelText: "{valueY}"
+                })
+            })
+        );
+        this.linkReviewSeries = chart.series.push(
+            am5xy.LineSeries.new(root, {
+                name: "Lined files",
+                xAxis: xAxis,
+                yAxis: yAxis,
+                valueYField: "value",
+                valueXField: "date",
+                stroke: am5.color("#ffb55a"),
                 tooltip: am5.Tooltip.new(root, {
                     labelText: "{valueY}"
                 })
@@ -46,34 +73,17 @@ export class ChartConfiguration {
 
         this.repoSeries = chart.series.push(
             am5xy.LineSeries.new(root, {
-                name: "Series",
+                name: "Repository",
                 xAxis: xAxis,
                 yAxis: yAxis,
                 valueYField: "value",
                 valueXField: "date",
+                stroke: am5.color("#b2e061"),
                 tooltip: am5.Tooltip.new(root, {
                     labelText: "{valueY}"
                 })
             })
         );
-
-        this.sumSeries = chart.series.push(
-            am5xy.LineSeries.new(root, {
-                name: "Series",
-                xAxis: xAxis,
-                yAxis: yAxis,
-                valueYField: "value",
-                valueXField: "date",
-                tooltip: am5.Tooltip.new(root, {
-                    labelText: "{valueY}"
-                })
-            })
-        );
-        this.sumSeries.strokes.template.setAll({
-            strokeWidth: 3,
-            strokeDasharray: [10,5]
-          });
-        this.sumSeries.set("stroke", am5.color("#a6a6a6"));
 
         var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
         cursor.lineY.set("visible", false);
@@ -89,19 +99,20 @@ export class ChartConfiguration {
                 value: 0,
                 bullet: false
             });
-            this.sumData.push({
+            this.linkReviewData.push({
                 date: new Date(2023, 8, i * 7 + 1).getTime(),
                 value: 0,
                 bullet: false
             });
         }
 
-        this.sumSeries.bullets.push(function (root, series, dataItem) {
+        this.reviewSeries.bullets.push(function (root, series, dataItem) {
             // @ts-ignore
             if (dataItem.dataContext.bullet) {
                 var container = am5.Container.new(root, {});
                 var circle0 = container.children.push(am5.Circle.new(root, {
                     radius: 5,
+                    tooltipText:"Pick",
                     fill: am5.color(0xff0000)
                 }));
                 var circle1 = container.children.push(am5.Circle.new(root, {
@@ -130,37 +141,39 @@ export class ChartConfiguration {
                 })
             }
         })
+        legend.data.setAll(chart.series.values);
         this.reviewSeries.data.setAll(this.reviewData);
         this.repoSeries.data.setAll(this.repoData);
-        this.sumSeries.data.setAll(this.sumData);
+        this.linkReviewSeries.data.setAll(this.linkReviewData);
     }
-    recalculateMax(index: number) {
-        this.sumData[index].value =  this.repoData[index].value + this.reviewData[index].value
-
-        let maxValue = this.sumData[0].value;
+    recalculateMax() {
+        let maxValue = this.reviewData[0].value;
         let maxIndex = 0;
-        this.sumData.forEach((element, i) => {
+        this.reviewData.forEach((element, i) => {
             element.bullet = false;
             if (maxValue < element.value) {
                 maxValue = element.value;
                 maxIndex = i;
             }
         });
-        this.sumData[maxIndex].bullet = true;
-        this.sumSeries.data.setAll(this.sumData);
+        this.reviewData[maxIndex].bullet = true;
+        this.reviewSeries.data.setAll(this.reviewData);
     }
 
 
     updateReview(index: number, value: number) {
         this.reviewData[index].value = value
-        this.reviewSeries.data.setAll(this.reviewData);
-        this.recalculateMax(index);
+        this.recalculateMax();
     }
 
     updateRepo(index: number, value: number) {
         this.repoData[index].value = value
         this.repoSeries.data.setAll(this.repoData);
-        this.recalculateMax(index);
+    }
+
+    updateLinkReview(index: number, value: number) {
+        this.linkReviewData[index].value = value
+        this.linkReviewSeries.data.setAll(this.linkReviewData);
     }
 }
 export class Point {
